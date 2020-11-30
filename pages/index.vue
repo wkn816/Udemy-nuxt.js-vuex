bef82c9b7f10342cb6bed0143acf6c75f02e4029
wkn816 <wkn816@gmail.com>
2020-11-30 21:36:33 +0900

    ミューテーションで渡すテキスト変更

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,8 @@
   <section class="container">
     <div>
       <p>{{ $store.state.message}}</p>
-      <button v-on:click="$store.commit('updateMessage')">update </button>
+      <!-- <button v-on:click="$store.commit('updateMessage')">update </button> -->
+      <button v-on:click="$store.commit('updateMessage', 'commit with payload')">update </button>
     </div>
   </section>
 </template>
